e0ca8eb9a06ead5c9fa1ab9d971a7bf6ff61e3ca
dhirajmistry98 <dhirajmistry168@gmail.com>
2025-12-16 13:12:03 +0530

    shadcn-ui

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ThemeToggle }from '@/components/ui/theme-toggle';
+
+export default function Home() {
+  return (
+ <div>
+    <h1>Welcome to the Home Page</h1>
+    <ThemeToggle/>
+ </div>
+  );
+}
+
